73f93b8a076dfd310e70cbd6ba169b698144e6a1
Takahiro Suzuki <tkasuz@amazon.co.jp>
2022-06-04 14:50:14 +0900

    first commit

---
+++ src/main/kotlin/com/kopkunka55/Application.kt
@@ -0,0 +1,13 @@
+package com.kopkunka55
+
+import io.ktor.server.application.*
+import com.kopkunka55.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+}
---
+++ src/main/kotlin/com/kopkunka55/plugins/Routing.kt
@@ -0,0 +1,22 @@
+package com.kopkunka55.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<AuthenticationException> { call, cause ->
+            call.respond(HttpStatusCode.Unauthorized)
+        }
+        exception<AuthorizationException> { call, cause ->
+            call.respond(HttpStatusCode.Forbidden)
+        }
+
+    }
+    routing {
+    }
+}
