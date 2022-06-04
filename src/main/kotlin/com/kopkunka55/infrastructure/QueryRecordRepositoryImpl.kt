e5a54d91ec7dd61ea4210a812979df3a6e1b6f91
Takahiro Suzuki <tkasuz@amazon.co.jp>
2022-06-05 00:08:28 +0900

    fix timezone bug by using exposed-java-time

--- src/main/kotlin/com/kopkunka55/infrastructure/QueryRecordRepositoryImpl.kt
+++ src/main/kotlin/com/kopkunka55/infrastructure/QueryRecordRepositoryImpl.kt
@@ -2,19 +2,20 @@ package com.kopkunka55.infrastructure
 
 import com.kopkunka55.domain.QueryRecord
 import com.kopkunka55.repository.QueryRecordRepository
-import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
-import org.jetbrains.exposed.dao.IntIdTable
-import org.jetbrains.exposed.sql.CustomDateTimeFunction
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.and
-import org.joda.time.DateTime
-import org.joda.time.DateTimeZone
+import org.jetbrains.exposed.sql.javatime.datetime
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import java.util.TimeZone
 
 object QueryRecordTable: IntIdTable("record"){
-    val datetime = datetime("datetime")
+    var datetime = datetime("datetime").uniqueIndex()
     val amount = float("amount")
 }
 
@@ -25,19 +26,21 @@ class QueryRecordEntity(id: EntityID<Int>): IntEntity(id){
 }
 
 class QueryRecordRepositoryImpl: QueryRecordRepository{
-    private fun String.toDateTimeAtUTC(): DateTime{
-        return DateTime(this).withZone(DateTimeZone.UTC)
+    private fun String.toDateTimeAtUTC(): LocalDateTime{
+        val dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX")
+        return LocalDateTime.parse(this, dtf)
     }
-    private fun DateTime.formatWithTimeZone(): String{
-        val datetime = ZonedDateTime.parse(this.toString())
-        val dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ssXXX")
-        return datetime.format(dtf)
+    private fun LocalDateTime.toUTCDateTimeString(): String{
+        val datetime = LocalDateTime.parse(this.toString())
+        val dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+        return datetime.format(dtf) + "+00:00"
     }
+
     override fun getRecordsBetweenDates(startDateTime: String, endDateTime: String): List<QueryRecord> {
         val queryRecordEntity = QueryRecordEntity.find {
-            (QueryRecordTable.datetime greaterEq startDateTime.toDateTimeAtUTC()) and
+            (QueryRecordTable.datetime greaterEq  startDateTime.toDateTimeAtUTC()) and
                     (QueryRecordTable.datetime lessEq endDateTime.toDateTimeAtUTC())
         }
-        return queryRecordEntity.map { QueryRecord(it.datetime.formatWithTimeZone(), it.amount) }
+        return queryRecordEntity.map { QueryRecord(it.datetime.toUTCDateTimeString(), it.amount) }
     }
 }
--- src/main/resources/db/migration/V2.01__craete_table.sql
+++ src/main/resources/db/migration/V2.01__craete_table.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS record;
 
+SET TIME ZONE 'UTC';
+
 CREATE TABLE record (
     id serial,
     datetime TIMESTAMP UNIQUE,
