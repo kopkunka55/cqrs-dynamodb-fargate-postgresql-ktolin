0f40d7d2f41047afa14e35d384bf0a6acfe1de36
Takahiro Suzuki <tkasuz@amazon.co.jp>
2022-06-04 16:21:01 +0900

    add record controller as router with some tests

--- src/main/kotlin/com/kopkunka55/plugins/Routing.kt
+++ src/main/kotlin/com/kopkunka55/plugins/Routing.kt
@@ -6,6 +6,13 @@ import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Record(val datetime: String, val amount: Float)
+
+@Serializable
+data class HistoryRequest(val startDateTime: String, val endDateTime: String)
 
 fun Application.configureRouting() {
     install(StatusPages) {
@@ -16,6 +23,22 @@ fun Application.configureRouting() {
             get {
                 call.respondText("OK")
             }
+        }
+        route("/wallet"){
+            post {
+                val request = call.receive<Record>()
+                val requestId = call.request.headers["X-Request-Id"].toString()
+                call.respond(Record(request.datetime, request.amount))
+            }
+            post("/search") {
+               val req = call.receive<HistoryRequest>()
+                call.respond(listOf(
+                    Record(req.startDateTime, 0.3.toFloat()) ,
+                    Record(req.startDateTime, 0.3.toFloat()) ,
+                    Record(req.startDateTime, 0.3.toFloat()) ,
+                ))
+            }
+
         }
     }
 }
